32619d8f04494b7dcb0ca0bb1ae7d14180ab1a47
Tim Stirrat <tstirrat@google.com>
2016-05-23 10:34:48 +1000

    remove console logs

--- addon/services/firebase.js
+++ addon/services/firebase.js
@@ -2,7 +2,6 @@ import firebase from 'firebase';
 
 export default {
   create() {
-    console.log('init app');
     if (!this.config.firebase || typeof this.config.firebase !== 'object') {
       throw new Error('Please set the `firebase` property in your environment config.');
     }
